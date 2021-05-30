cd177beca33c9b5269e5963c0a49766c5fac5722
Siri <madhusindhu5@gmail.com>
2021-05-29 20:04:59 -0500

    Play a prank and swap some files!

--- SwapFile.py
+++ SwapFile.py
@@ -1,3 +1,11 @@
-def swapfiles():
-    print("dj")
-swapfiles()
+def swapfile():
+    with open(r"sample1.txt", 'r') as a:
+        data_a = a.read()
+    with open(r"sample2.txt", 'r') as b:
+        data_b = b.read()
+    with open(r"sample1.txt", 'w') as a:
+        a.write(data_b)
+    with open(r"sample2.txt", 'w') as b:
+        b.write(data_a)
+
+swapfile()
